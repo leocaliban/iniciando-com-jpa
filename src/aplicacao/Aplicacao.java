45d8e89d982192fb0c829a4a6675f26f5ddcc098
Leocaliban <leocaliban@gmail.com>
2018-01-13 14:26:21 -0300

    Cria a conexão com o banco de dados

--- src/aplicacao/Aplicacao.java
+++ src/aplicacao/Aplicacao.java
@@ -1,4 +1,8 @@
 package aplicacao;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Aplicacao {
@@ -7,8 +11,13 @@ public class Aplicacao {
 	
 		Pessoa pessoa1 = new Pessoa(1L, "Jack Bauer", "jb24@gmail.com");
 		Pessoa pessoa2 = new Pessoa(2L, "Nina Meyers", "nina@gmail.com");
-		Pessoa pessoa3 = new Pessoa(3L, "Ramón Salazar", "donramon@gmail.com");
+		Pessoa pessoa3 = new Pessoa(3L, "Ramï¿½n Salazar", "donramon@gmail.com");
 		Pessoa pessoa4 = new Pessoa(4L, "David Palmer", "david_palmer@gmail.com");
+		
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		
+		
 		System.out.println(pessoa1);
 		System.out.println(pessoa2);
 		System.out.println(pessoa3);
