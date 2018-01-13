9bf6c5f2629e26149938631f85b36ab6fd87c9e4
Leocaliban <leocaliban@gmail.com>
2018-01-13 04:10:39 -0300

    Realiza o mapeamento das entidades

--- src/dominio/Pessoa.java
+++ src/dominio/Pessoa.java
@@ -1,10 +1,22 @@
 package dominio;
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+
+@Entity
 public class Pessoa implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column(name="nomecompleto")
 	private String nome;
 	private String email;
 	
